b0d1c52a32b4ca9aefc3f49e8b5c5c242be44376
Leo <414563035@qq.com>
2024-03-23 21:22:42 +0800

    add comment

--- money.go
+++ money.go
@@ -1,9 +1,14 @@
+// @Since 2024-03-22.
+// @Author Leo, All rights Reserved.
+
 package validator
 
 import "regexp"
 
 var moneyRegexp = regexp.MustCompile("^([0-9]+|[0-9]{1,3}(,[0-9]{3})*)(.[0-9]{1,2})?$")
 
+// IsValidMoney 金钱
+// Validate is the money a valid money
 func IsValidMoney(money string, allowBlank bool) bool {
 	if value, ok := testBlank(money); ok {
 		return moneyRegexp.MatchString(value)
