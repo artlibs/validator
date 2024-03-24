e698402174502f0ec1fbd68210de81451f25b6f7
Fury <i36.lib@gmail.com>
2024-03-24 17:56:24 +0800

    add test case

--- idcard_test.go
+++ idcard_test.go
@@ -3,11 +3,30 @@
 
 package validator
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestIsValidIdCard(t *testing.T) {
-	// 15
+	assert.True(t, IsValidIdCard("", true))
+	assert.False(t, IsValidIdCard("", false))
 
-	// 18
+	validIdCards := []string{
+		"510212197002066715", // zkh
+		"612321198301063313", // zkk
+	}
+	for _, id := range validIdCards {
+		assert.True(t, IsValidIdCard(id, true))
+		assert.True(t, IsValidIdCard(id, false))
+	}
 
+	invalidIdCards := []string{
+		"510212197002066711",
+		"612321198301063311",
+	}
+	for _, id := range invalidIdCards {
+		assert.False(t, IsValidIdCard(id, true))
+		assert.False(t, IsValidIdCard(id, false))
+	}
 }
--- phone_test.go
+++ phone_test.go
@@ -3,16 +3,83 @@
 
 package validator
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestIsValidPhone(t *testing.T) {
-	// basic
+	// test empty
+	pts := []PhoneType{Basic, Virtual, NetOnly, IotOnly, AllType}
+	for _, pt := range pts {
+		assert.True(t, IsValidPhone("", true, []PhoneType{pt}))
+		assert.False(t, IsValidPhone("", false, []PhoneType{pt}))
+	}
 
-	// virtual
+	// test phones
+	caseMap := map[string]map[PhoneType]bool{
+		"18157610011": { // valid basic
+			Basic:   true,
+			Virtual: false,
+			NetOnly: false,
+			IotOnly: false,
+			AllType: true,
+		},
+		"16577810902": { // valid virtual
+			Basic:   false,
+			Virtual: true,
+			NetOnly: false,
+			IotOnly: false,
+			AllType: true,
+		},
+		"14577812563": { // valid net
+			Basic:   false,
+			Virtual: false,
+			NetOnly: true,
+			IotOnly: false,
+			AllType: true,
+		},
+		"1440775619012": { // valid iot
+			Basic:   false,
+			Virtual: false,
+			NetOnly: false,
+			IotOnly: true,
+			AllType: true,
+		},
+		"123456": { // all invalid
+			Basic:   false,
+			Virtual: false,
+			NetOnly: false,
+			IotOnly: false,
+			AllType: false,
+		},
+		"181778189012": { // all invalid
+			Basic:   false,
+			Virtual: false,
+			NetOnly: false,
+			IotOnly: false,
+			AllType: false,
+		},
+		"1817781890123": { // all invalid
+			Basic:   false,
+			Virtual: false,
+			NetOnly: false,
+			IotOnly: false,
+			AllType: false,
+		},
+		"181ab7781cd": { // all invalid
+			Basic:   false,
+			Virtual: false,
+			NetOnly: false,
+			IotOnly: false,
+			AllType: false,
+		},
+	}
+	for phone, kv := range caseMap {
+		for pt, expect := range kv {
+			assert.Equal(t, expect, IsValidPhone(phone, false, []PhoneType{pt}))
+			assert.Equal(t, expect, IsValidPhone(phone, true, []PhoneType{pt}))
+		}
+	}
 
-	// net
-
-	// iot
-
-	// all
 }
