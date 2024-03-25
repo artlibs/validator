402c4977f26e067d8dbafe9e1d1e6de41484a694
Fury <i36.lib@gmail.com>
2024-03-25 17:51:24 +0800

    rename TagMaps --> TagMap

--- tags.go
+++ tags.go
@@ -2,9 +2,9 @@ package validator
 
 type Fn func(value string, allowEmpty bool) bool
 
-// TagMaps tags map, when you want to add a new validation to the validator
+// TagMap: tags map, when you want to add a new validation to the validator
 // you can range this map to get the tag and valid function
-var TagMaps = map[string]Fn{
+var TagMap = map[string]Fn{
 	"email":      IsValidEmail,
 	"http_url":   IsValidHttpURL,
 	"id_card":    IsValidIdCard,
